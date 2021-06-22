1cc5b75ee1884b42ee52f168410bbdc9ff2c2e37
Jetze Sikkema <jhsikkema@gmail.com>
2021-06-22 12:48:31 +0200

    Improved examples

---
+++ webui/examples.php
@@ -0,0 +1,31 @@
+<?php
+$examples = [array("title"=>"NYC Coroner That Declared Epstein’s Death ‘Suicide’ Was on Clinton Foundation Payroll Until 2015",
+	    "publisher"=>"Taters Gonna Tate",
+	    "author"=>"Flagg Eagleton-Patriot",
+	    "article content"=>"An independent investigation by credible sources that may or may not be tied to the federal task force to investigate the Clintons has found that the New York City coroner who signed Jeffrey Epstein’s death cerificate was on the Clinton Foundation’s Board of Trustees until 2015. This report, which is an alleged excerpt from the Foundation’s public filings, has not been doctored at all. It shows clearly that Eric Braverman, the same man who declared Epstein’s death a suicide, made nearly $500K in 2015 for a cushy 45-hour per week job. At first, our investigators were skeptical, because the Coroner’s name isn’t technically \"Eric Braverman\" at all. It’s Dr. Arturo Tubbolski under the assumed name of Greg. Our investigator in the field, Skip Tetheluda, explains\" Greg was first hired as Art, but then his roommate transitioned from Leon to Linda, which made him a little uncomfortable. He moved in with Chelsea Clinton’s best friend from Dartmouth and before long he had taken over the position of Eric Braverman, the Foundation’s chief Operating Officer. We believe the real Braverman is buried wherever they put Vince Foster and Dimitri Noonan. Anyway, the fact that both men are listed makes this a slam dunk, yet the New York Attorney General, Sandiford Batt, won’t press charges. It’s a clear-cut caes of the Deep State covering their crimes.” Well if that doesn’t make perfect sense then nothing does. At this point, this Greg character needs to be found and locked up. According to the City of New York, he never worked there at all. Because Epstein has no family and left no will, there’s nobody to contest it. At least it’s finally over. We’ll never know how many of Clinton’s 26 trips were actually true now.",
+	    "ipfs"=>"",
+	    "fake"=>True),
+	    array("title"=>"Biden Calls Trump and Supporters “Dregs of Society”",
+	    "publisher"=> "",
+	    "author"=>"Corrie O'Connor",
+	    "article_content"=>"Former Vice President Joe Biden and his wife addressed a heavily Democratic crowd at the annual Human Rights Campaign gala in Washington D.C. over the weekend.
+
+In it, Biden asked God to forgive him for not speaking out against President Trump.
+
+He went on to claim “The President uses the White House as a literal…bully pulpit.”
+
+During the fundraising gala on Saturday night to a crowd of primarily LGBT supporters, Biden also called Trump staff and supporters, “Some of…the dregs of society,” adding that they have an ally currently in the White House.
+
+Biden has said he'll decide in January - after the midterms - whether to make a run against Trump in 2020.
+
+Corrie O'Connor",
+       "ipfs"=>"QmTQZnNETmnDyk4z6mLUXinQsa71JhE7tHM8wCcY6vn1x6",
+       "fake"=>True)
+
+
+
+];
+
+echo json_encode($examples);
+
+?>
